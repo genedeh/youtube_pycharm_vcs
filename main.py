0168dbdb352febc67f69f6b0a977779aeb52fa7f
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 16:50:09 +0100

    importing password

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from secret import password
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
